c9897e7f74f217db78ac7c835735aa43ed449e08
Bianca Espíndola <32000570+BiancaEspindola@users.noreply.github.com>
2021-09-17 14:39:44 -0300

    objetos 2

--- 03objetos.js
+++ 03objetos.js
@@ -8,9 +8,11 @@ console.log('propriedade name do objeto user:', user.name);
 user.name = 'Bibi';
 console.log('propriedade name do objeto user:', user.name);
 
+//como array
 user['name'] = 'outra bibi';
 console.log('propriedade name do objeto user:', user.name);
 
+//como uma variavel
 const prop = 'name';
 user[prop] = 'outra bibi 2.0';
 console.log('propriedade name do objeto user:', user.name);
@@ -18,7 +20,13 @@ console.log('propriedade name do objeto user:', user.name);
 //criando uma propriedade
 user.lastName = 'de Espindola';
 console.log('propriedade lastName do objeto user:', user.lastName);
+console.log('chaves do objeto user:', Object.keys(user));
+console.log('valores do objeto user:', Object.values(user));
+console.log('entradas do objeto user:', Object.entries(user));
+
 
 //deletando uma propriedade de
 delete user.name;
+console.log('propriedades do objeto user:', Object.keys(user));
+
 
