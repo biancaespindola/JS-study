2410d6165306565ab09c94f60c4676ecdb5854cc
Bianca Espíndola <32000570+BiancaEspindola@users.noreply.github.com>
2021-09-21 11:12:46 -0300

    tipos primitivos

---
+++ JavascriptIntro/aula19.js
@@ -0,0 +1,19 @@
+/**
+ * string
+ * int
+ * float
+ */
+
+let name ='Bianca';
+let num = 12;
+let num2 = 12.3;
+let nulo = null;
+let naoDef;
+let boolean = true;
+
+console.log(name + ' do tipo: ' + typeof name);
+console.log(num + ' do tipo: ' + typeof num);
+console.log(num2 + ' do tipo: ' + typeof num2);
+console.log(nulo + ' do tipo: ' + typeof nulo);
+console.log(naoDef + ' do tipo: ' +typeof naoDef);
+console.log(boolean + ' do tipo: ' +typeof boolean);
