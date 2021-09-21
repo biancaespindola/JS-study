c1dcbd2c702e6266979d36f9b1ad4a7283dec771
Bianca Espíndola <32000570+BiancaEspindola@users.noreply.github.com>
2021-09-21 11:52:43 -0300

    alert confirm prompt

---
+++ JavascriptIntro/aula21/js/script.js
@@ -0,0 +1,11 @@
+/**
+ * alert confirm e prompt
+*/
+alert("mensagem!!!");
+//não tem retorno
+
+confirm('Deseja realmente cancelar?');
+// tem retorno de verdadeiro ou falso
+
+prompt("Digite o seu nome:");
+//recebe o que o usuario digitar na caixa
